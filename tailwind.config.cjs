0892261b09dcef5bf4c1f06c4a5de253ae232077
hieu1966hn <hieu1966hn@gmail.com>
2023-01-20 19:55:42 +0700

    new code L2E_FE

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        yellow: "#FFAA01",
+        red: "#C00000",
+        orange: "#FF7427",
+      },
+    },
+  },
+  plugins: [],
+};
